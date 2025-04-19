3bc91bdebb91074dd48ec8a58ff90356756e180e
Simon Lund <simon97lund@gmail.com>
2025-04-19 18:16:53 +0200

    :heavy_plus_sign: add shadcn-svelte including relevant packages

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+// TODO: Remove this once shadcn-svelte does not require it
+
+/** @type {import('tailwindcss').Config} */
+export default {
+	darkMode: ["class"],
+	content: ["./src/**/*.{html,js,svelte,ts}"],
+	safelist: ["dark"],
+}
