05f723df0c28740d36226256174f2ad9c2a64699
Simon Lund <simon97lund@gmail.com>
2025-04-27 18:14:59 +0200

    :bug::wrench: fix static rendering config

--- svelte.config.js
+++ svelte.config.js
@@ -8,13 +8,11 @@ const config = {
 	preprocess: vitePreprocess(),
 	kit: {
 		adapter: adapter({
-			// default options are shown. On some platforms
-			// these options are set automatically — see below
 			pages: 'build',
 			assets: 'build',
 			fallback: undefined,
 			precompress: false,
-			strict: false
+			strict: true
 		}),
 		paths: {
 			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
