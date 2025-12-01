c9f1eae92161c29c0dff907f5b48814fda6b2604
Simon Lund <simon97lund@gmail.com>
2025-12-01 23:09:37 +0100

    :children_crossing: decouple search from input to prevent lagging

--- src/lib/components/header/header-search.svelte
+++ src/lib/components/header/header-search.svelte
@@ -3,6 +3,18 @@
 	import { LoaderCircleIcon, SearchIcon } from '@lucide/svelte';
 	import { inputVariants } from '$lib/components/ui/input';
 	import { queryParams } from '$lib/hooks/query-params.svelte.js';
+	import { useThrottle } from 'runed';
+	import { onMount } from 'svelte';
+
+	let search = $state('');
+	const throttledUpdate = useThrottle(() => {
+		queryParams.search = search || null;
+	}, 500);
+
+	onMount(() => {
+		// Initialize search from query params on mount
+		search = queryParams.search || '';
+	});
 </script>
 
 <label class={inputVariants({ class: 'rounded-full' })}>
@@ -15,7 +27,13 @@
 		id="search"
 		name="search"
 		aria-label="Search libraries"
-		bind:value={queryParams.search}
+		bind:value={
+			() => search,
+			(v) => {
+				search = v;
+				throttledUpdate();
+			}
+		}
 		class="w-full border-none bg-transparent focus:outline-none"
 		type="search"
 		placeholder="Search libraries..."
