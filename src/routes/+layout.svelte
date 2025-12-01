a3274973338b9f953a6369f60eb407c572fe18d9
Simon Lund <simon97lund@gmail.com>
2025-12-02 00:29:56 +0100

    :chart_with_upwards_trend: add setup for analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,18 @@
 <script lang="ts">
 	import '../app.css';
 	import { ModeWatcher } from 'mode-watcher';
+	import { dev } from '$app/environment';
+	import { PUBLIC_ANALYTICS_ID, PUBLIC_ANALYTICS_SCRIPT_URL } from '$env/static/public';
 
 	let { children } = $props();
 </script>
+
+<svelte:head>
+	<!-- Add analytics	-->
+	{#if !dev}
+		<script defer src={PUBLIC_ANALYTICS_SCRIPT_URL} data-website-id={PUBLIC_ANALYTICS_ID}></script>
+	{/if}
+</svelte:head>
 
 <ModeWatcher />
 {@render children?.()}
